b04d8dd4107ec4d730b0f82f230e89aaff4d2064
Dave Webb <dave-riroriro@dcw.co.nz>
2021-02-14 12:28:32 +1300

    Working (I think) entropy and length calcs. TODO: alphabet size

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 // pp-entropy
 // Given two of alphabet size, passphrase length, and bits of entropy,
 // calculate the third.
-
+// - entropy       = log2 ( alphabet size ** length )
+// - length        = 2 ** entropy / log( alphabet size )
+// - alphabet size = 2 ** entropy *
+//                  x        = log2 ( a ** b )
+//		    2 ** x   = a ** b
+//                  (2 ** x) / log(b) = a ???
 import (
 	"flag"
 	"fmt"
+	"math"
 	"os"
 )
 
@@ -17,19 +23,26 @@ func usage() {
 
 }
 func main() {
-
+	flag.Usage = usage
 	alphabet := flag.Int("alpha", 0, "Size of alphabet (words or chars)")
 	length := flag.Int("len", 0, "Length of passphrase (words or chars)")
 	entropy := flag.Float64("ent", 0, "Required entropy (bits)")
-
-	flag.Usage = usage
 	flag.Parse()
-	fmt.Println("A=", *alphabet)
-	fmt.Println("L=", *length)
-	fmt.Println("E=", *entropy)
 
-	if *entropy > 10.0 {
-		fmt.Println("ent 10")
+	if *alphabet > 0 && *length > 0 && *entropy == 0.0 {
+		fmt.Printf("An alphabet of %d and a length of %d gives entropy of %0.1f bits\n",
+			*alphabet,
+			*length,
+			math.Log2(math.Pow(float64(*alphabet), float64(*length))))
+	} else if *alphabet > 0 && *entropy > 0.0 && *length == 0 {
+		fmt.Printf("An alphabet of %d and an entropy of %0.1f bits requires a length of %d\n",
+			*alphabet,
+			*entropy,
+			int(math.Ceil(math.Log(math.Pow(2, *entropy))/math.Log(float64(*alphabet)))))
+	} else if *length > 0 && *entropy > 0.0 && *alphabet == 0.0 {
+		fmt.Printf("An entropy of %0.1f bits and a length = %d requires a alphabet size of %d\n",
+			*entropy,
+			*length,
+			int(math.Ceil(math.Log(math.Pow(2, *entropy))/math.Log(float64(*length)))))
 	}
-	flag.Usage()
 }
