e3d2a8746e3ce3134b9c94e48fcd9ec4416882e2
Dave Webb <dave-riroriro@dcw.co.nz>
2021-02-14 10:30:17 +1300

    Usage message and basic arg parsing

--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 // pp-entropy
-// Given two of alphabet size, passphrase lenght, and bits of entropy,
+// Given two of alphabet size, passphrase length, and bits of entropy,
 // calculate the third.
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
-var alphabet = flag.Int("alpha", 0, "Size of alphabet (words or chars)")
-var length = flag.Int("len", 0, "Length of passphrase (words or chars)")
-var entropy = flag.Float64("ent", 0, "Required entropy (bits)")
+func usage() {
+	fmt.Fprintln(os.Stderr, "pp-entropy - random passphrase strength calculator")
+	fmt.Fprintln(os.Stderr, "Given two of alphabet size, passphrase length, and bits of entropy, calculate the third.")
+	flag.PrintDefaults()
 
+}
 func main() {
+
+	alphabet := flag.Int("alpha", 0, "Size of alphabet (words or chars)")
+	length := flag.Int("len", 0, "Length of passphrase (words or chars)")
+	entropy := flag.Float64("ent", 0, "Required entropy (bits)")
+
+	flag.Usage = usage
 	flag.Parse()
+	fmt.Println("A=", *alphabet)
+	fmt.Println("L=", *length)
+	fmt.Println("E=", *entropy)
+
+	if *entropy > 10.0 {
+		fmt.Println("ent 10")
+	}
+	flag.Usage()
 }
